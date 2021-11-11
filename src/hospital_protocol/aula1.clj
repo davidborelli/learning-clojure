ce2dd5082bbfe510a32f33e5fe01b929daf1fb1b
David Borelli <david_borelli@live.com>
2021-11-11 10:23:55 -0300

    using Java interoperability, and using record

--- src/hospital_protocol/aula1.clj
+++ src/hospital_protocol/aula1.clj
@@ -19,5 +19,35 @@
     (pprint (adiciona-paciente pacientes erick))
     ))
 
-(testa-uso-de-pacientes)
+;(testa-uso-de-pacientes)
+
+(defrecord Paciente [id, nome, nascimento])
+
+(println (->Paciente 15 "David Borelli" "27/09/1989"))
+(pprint (->Paciente 15 "David Borelli" "27/09/1989"))
+(pprint (Paciente. 15 "David" "27/10/95"))
+(pprint (Paciente. "David" 15 "27/10/95"))
+(pprint (Paciente. 15 "27/10/95" "David"))
+(pprint (class (Paciente. 15 "27/10/95" "David")))
+(pprint (map->Paciente {:id 12, :nome "David", :nascimento "18/09/1989"}))
+
+(let [david (->Paciente 15 "David Borelli" "27/09/1989")]
+  (println (get david :id))
+  (println (vals david))
+  (println (class david))
+  (println (record? david))
+  (println (.nome david)))
+
+(pprint (map->Paciente {:id 12, :nome "David", :nascimento "18/09/1989" :rg "22222222"}))
+;(pprint (Paciente. "David" "27/09/1989"))
+(pprint (Paciente. nil "David" "27/09/1989"))
+(pprint (map->Paciente {:nome "David", :nascimento "27/09/10", :rg "22222222"}))
+
+(pprint (assoc (Paciente. nil "David" "27/09/1989") :id 38))
+(pprint (class (assoc (Paciente. nil "David" "27/09/1989") :id 38)))
+
+;(Thread/sleep)
+
+(pprint (= (->Paciente 15 "David Borelli" "27/09/1989") (->Paciente 15 "David Borelli" "27/09/1989")))
+(pprint (= (->Paciente 152 "David Borelli" "27/09/1989") (->Paciente 15 "David Borelli" "27/09/1989")))
 
